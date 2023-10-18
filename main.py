f18a70bc2dce8321c03a73be5a74368697289e79
AnnZhimol <ankavanka731@gmail.com>
2023-10-18 18:27:01 +0400

    изменения 2

--- main.py
+++ main.py
@@ -6,6 +6,35 @@ import numpy as np
 
 app = Flask(__name__)
 
+def new_df():
+    df=pd.read_csv('ds_salaries.csv')
+
+    for i in range(len(df),round(len(df)*1.1)+1):
+        experience_level = df['experience_level'].value_counts().index[random.randint(0,2)]
+        employment_type=df['employment_type'].value_counts().index[random.randint(0,2)]
+        job_title=df['job_title'].value_counts().index[random.randint(0,2)]
+        salary_currency=df['salary_currency'].value_counts().index[random.randint(0,2)]
+        employee_residence=df['employee_residence'].value_counts().index[random.randint(0,2)]
+        remote_ratio=df['remote_ratio'].value_counts().index[random.randint(0,2)]
+        company_location=df['company_location'].value_counts().index[random.randint(0,2)]
+        company_size=df['company_size'].value_counts().index[random.randint(0,2)]
+        work_year=df['work_year'].value_counts().index[random.randint(0,2)]
+
+        max_salary=df['salary'].max()
+        min_salary=df['salary'].min()
+        avg_salary=df['salary'].mean()
+        new_salary=round(avg_salary + random.uniform(min_salary - avg_salary, max_salary - avg_salary))
+
+        max_salary_usd=df['salary_in_usd'].max()
+        min_salary_usd=df['salary_in_usd'].min()
+        avg_salary_usd=df['salary_in_usd'].mean()
+        new_salary_usd=round(avg_salary_usd + random.uniform(min_salary_usd - avg_salary_usd, max_salary_usd - avg_salary_usd))
+
+        new_row=[i,work_year,experience_level,employment_type,job_title,new_salary,salary_currency,new_salary_usd,employee_residence,remote_ratio,company_location,company_size]
+        df.loc[i]=new_row
+
+    df.to_csv('new_ds_salaries.csv',index=False)
+
 def graphic(category,val):
     df_One=pd.read_csv('new_ds_salaries.csv').groupby(category).agg(salary_in_usd=('salary_in_usd', val)).reset_index()
     df_Two=pd.read_csv('ds_salaries.csv').groupby(category).agg(salary_in_usd=('salary_in_usd', val)).reset_index()
@@ -61,31 +90,3 @@ def data():
 if __name__=="__main__":
     app.run(debug=True, threaded=True)
 
-def new_df():
-    df=pd.read_csv('ds_salaries.csv')
-
-    for i in range(len(df),round(len(df)*1.1)+1):
-        experience_level = df['experience_level'].value_counts().index[random.randint(0,2)]
-        employment_type=df['employment_type'].value_counts().index[random.randint(0,2)]
-        job_title=df['job_title'].value_counts().index[random.randint(0,2)]
-        salary_currency=df['salary_currency'].value_counts().index[random.randint(0,2)]
-        employee_residence=df['employee_residence'].value_counts().index[random.randint(0,2)]
-        remote_ratio=df['remote_ratio'].value_counts().index[random.randint(0,2)]
-        company_location=df['company_location'].value_counts().index[random.randint(0,2)]
-        company_size=df['company_size'].value_counts().index[random.randint(0,2)]
-        work_year=df['work_year'].value_counts().index[random.randint(0,2)]
-
-        max_salary=df['salary'].max()
-        min_salary=df['salary'].min()
-        avg_salary=df['salary'].mean()
-        new_salary=round(avg_salary + random.uniform(min_salary - avg_salary, max_salary - avg_salary))
-
-        max_salary_usd=df['salary_in_usd'].max()
-        min_salary_usd=df['salary_in_usd'].min()
-        avg_salary_usd=df['salary_in_usd'].mean()
-        new_salary_usd=round(avg_salary_usd + random.uniform(min_salary_usd - avg_salary_usd, max_salary_usd - avg_salary_usd))
-
-        new_row=[i,work_year,experience_level,employment_type,job_title,new_salary,salary_currency,new_salary_usd,employee_residence,remote_ratio,company_location,company_size]
-        df.loc[i]=new_row
-
-    df.to_csv('new_ds_salaries.csv',index=False)

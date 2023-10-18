13a1a314cddeb9c7511cc7aaa39d2e12a93ea3ec
AnnZhimol <ankavanka731@gmail.com>
2023-10-18 18:08:27 +0400

    lab3 complete

--- main.py
+++ main.py
@@ -1,31 +1,65 @@
-#from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
-def graphic():
-    df=pd.read_csv('new_ds_salaries.csv')
+app = Flask(__name__)
 
-    d = df.groupby('job_title').agg(salary_in_usd=('salary_in_usd', 'max')).reset_index()
-    d['salary_in_usd'] = d['salary_in_usd'].apply(lambda x: round(x, 1))
-    plt.xlabel('Профессии')
-    plt.ylabel('Средняя зарплата в долларах')
-    barplot = plt.bar(x=d['job_title'], height=d['salary_in_usd'])
-    plt.bar_label(barplot, labels=d['salary_in_usd'])
-    plt.xticks(rotation=90)
+def graphic(category,val):
+    df_One=pd.read_csv('new_ds_salaries.csv').groupby(category).agg(salary_in_usd=('salary_in_usd', val)).reset_index()
+    df_Two=pd.read_csv('ds_salaries.csv').groupby(category).agg(salary_in_usd=('salary_in_usd', val)).reset_index()
+    df_One['salary_in_usd'] = df_One['salary_in_usd'].apply(lambda x: round(x, 1))
+    df_Two['salary_in_usd'] = df_Two['salary_in_usd'].apply(lambda x: round(x, 1))
 
-    df=pd.read_csv('ds_salaries.csv')
+    fig, ax = plt.subplots()
 
-    d = df.groupby('job_title').agg(salary_in_usd=('salary_in_usd', 'max')).reset_index()
-    d['salary_in_usd'] = d['salary_in_usd'].apply(lambda x: round(x, 1))
-    plt.xlabel('Профессии')
-    plt.ylabel('Средняя зарплата в долларах')
-    barplot = plt.bar(x=d['job_title'], height=d['salary_in_usd'])
-    plt.bar_label(barplot, labels=d['salary_in_usd'])
-    plt.xticks(rotation=90)
+    index = np.arange(len(df_One[category]))
+    bar_width = 0.35
+    opacity = 0.8
+
+    pps=ax.bar(index, df_One['salary_in_usd'], color='r', align='edge', width=bar_width, label = 'Новая таблица')
+    for p in pps:
+        height = p.get_height()
+        ax.annotate('{}'.format(height),
+        xy=(p.get_x() + p.get_width() / 2, height),
+        xytext=(0, 3), # 3 points vertical offset
+        textcoords="offset points",
+        ha='center', va='bottom')
+    
+    pps2=ax.bar(index, df_Two['salary_in_usd'], color='b', align='edge', width=-bar_width,label = 'Старая таблица')
+    for p in pps2:
+        height = p.get_height()
+        ax.annotate('{}'.format(height),
+        xy=(p.get_x() + p.get_width() / 2, height),
+        xytext=(0, 3), # 3 points vertical offset
+        textcoords="offset points",
+        ha='center', va='bottom')
+
+    # Assign the tick labels
+    ax.set_xticks(index)
+    ax.set_xticklabels(df_One[category], rotation=90)
+
+    plt.xlabel(category)
+    plt.ylabel(val)
+    plt.legend()
     plt.show()
 
-graphic()
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/data', methods=['get'])
+def data():
+    
+    valueForGroup = request.args.get('flexRadioDefault')
+    valueForVal = request.args.get('flexRadioDefault2')
+    graphic(valueForGroup,valueForVal)
+    
+    return render_template("data.html") 
+
+if __name__=="__main__":
+    app.run(debug=True, threaded=True)
 
 def new_df():
     df=pd.read_csv('ds_salaries.csv')
@@ -55,59 +89,3 @@ def new_df():
         df.loc[i]=new_row
 
     df.to_csv('new_ds_salaries.csv',index=False)
-
-#new_df()
-
-# app = Flask(__name__)
-
-# #главная страница
-# @app.route('/')
-# def index():
-#     return render_template("index.html")
-
-# #страница с данными
-# @app.route('/data', methods=['get'])
-# def data():
-#     #данные из input
-#     inputVal=request.args
-
-#     #данные из csv
-#     salaryData=pd.read_csv('ds_salaries.csv',sep=',')
-    
-#     #значения строк из input
-#     fromStr=int(inputVal['from_str'])
-#     toStr=int(inputVal['to_str'])
-
-#     #значения столбцов из input
-#     fromStl=int(inputVal['from_stl'])
-#     toStl=int(inputVal['to_stl'])
-
-#     #итоговая таблица
-#     outputData=salaryData.iloc[fromStr:toStr,fromStl:toStl]
-
-#     #количество пустых ячеек
-#     countNone=salaryData.isna().sum()
-#     #количество заполненных ячеек
-#     countFill=salaryData.count()
-#     #количество строк
-#     lenStr=len(salaryData.axes[0])
-#     #количество столбцов
-#     lenStl=len(salaryData.axes[1])
-#     #описание набора данных
-#     descriptionData='В наборе данных присутствует информация о заработной плате в области DataScience. ' \
-#                     'Здесь представлены наиболее полные данные, с помощью которых можно проанализировать заработные платы специалистов в зависимости от различных характеристик. '
-#     #информация о столбцах
-#     stlInfo=str(salaryData.dtypes)
-#     #таблица html
-#     tableHtml=outputData.to_html(classes='table table-striped')
-
-#     #проверка диапазона
-#     if (fromStr>toStl | fromStl>toStl):
-#         return '''<center><h1>Введен некорректный диапазон</h1></center>'''+render_template("index.html")
-#     else:
-#         return render_template("data.html", countNone=countNone, countFill=countFill, lenStr=lenStr, lenStl=lenStl,
-#                             descriptionData=descriptionData, stlInfo=stlInfo) \
-#                             + '''<div class="container mt-4"><div class="card"><div class="card-body" style="overflow: auto">'''+ tableHtml + '''</div></div></div>''' 
-
-# if __name__=="__main__":
-#     app.run(debug=True, threaded=True)
